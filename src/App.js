9291601d5a90f5d5c8707c2e63ed46a2e1e61446
Patricia Mae Quitoriano <87472224+patkyu@users.noreply.github.com>
2023-07-29 16:42:39 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import SignUp1 from "./SignUp1";
+import SignUp2 from "./SignUp2";
+import LandingPage from "./LandingPage";
+import LogIn1 from "./LogIn1";
+import LogIn2 from "./LogIn2";
+
+function DefaultRouteComponent() {
+  return <div></div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/sign-up-1" element={<SignUp1 />} />
+        <Route path="/sign-up-2" element={<SignUp2 />} />
+        <Route path="/log-in-1" element={<LogIn1 />} />
+        <Route path="/log-in-2" element={<LogIn2 />} />
+        {/* Redirect to /sign-up-1 when root URL is visited */}
+        <Route path="/" element={<Navigate to="/sign-up-1" />} />
+        {/* You can also use the DefaultRouteComponent for the root URL */}
+        <Route path="/" element={<DefaultRouteComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
